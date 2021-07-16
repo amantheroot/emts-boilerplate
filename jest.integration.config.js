4d2fd64b45147ec79c1958b78939a4b4043841de
amantheroot <amantheroot@gmail.com>
2021-07-16 15:17:53 +0530

    added tsc build

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/src/tests/integration', '<rootDir>/build'],
+  // testPathIgnorePatterns: ['build'],
+  modulePathIgnorePatterns: ['<rootDir>/build'],
+  testRegex: '/integration/',
   testEnvironmentOptions: {
     NODE_ENV: 'test',
+    APP_ENV: 'test',
   },
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
-};
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+}
