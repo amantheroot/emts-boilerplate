8e5755ef43922ae31fcc5f949ec0f6d752dbff13
amantheroot <amantheroot@gmail.com>
2021-07-20 12:41:19 +0530

    formatted all the files

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/tests/integration', '<rootDir>/build'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["<rootDir>/tests/integration", "<rootDir>/build"],
   testEnvironmentOptions: {
-    NODE_ENV: 'test',
+    NODE_ENV: "test",
   },
   restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  coveragePathIgnorePatterns: ["node_modules", "src/config", "src/app.ts", "tests"],
+  coverageReporters: ["text", "lcov", "clover", "html"],
 };
--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   // testPathIgnorePatterns: ['build'],
-  modulePathIgnorePatterns: ['<rootDir>/build'],
-  testRegex: '/integration/',
+  modulePathIgnorePatterns: ["<rootDir>/build"],
+  testRegex: "/integration/",
   testEnvironmentOptions: {
-    NODE_ENV: 'test',
-    APP_ENV: 'test',
+    NODE_ENV: "test",
+    APP_ENV: "test",
   },
   restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  coveragePathIgnorePatterns: ["node_modules", "src/config", "src/app.ts", "tests"],
+  coverageReporters: ["text", "lcov", "clover", "html"],
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
+    "@/(.*)": path.resolve(__dirname, "./src/$1"),
+    "@@/(.*)": path.resolve(__dirname, "./$1"),
   },
-}
+};
